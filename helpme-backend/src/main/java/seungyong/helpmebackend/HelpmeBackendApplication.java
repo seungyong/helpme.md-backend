a72d47ce78627d26827d6a65543cf85f909caa92
seungyong <seungyong20@naver.com>
2026-01-10 18:19:39 +0900

    기초 셋팅

---
+++ helpme-backend/src/main/java/seungyong/helpmebackend/HelpmeBackendApplication.java
@@ -0,0 +1,13 @@
+package seungyong.helpmebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpmeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpmeBackendApplication.class, args);
+    }
+
+}
