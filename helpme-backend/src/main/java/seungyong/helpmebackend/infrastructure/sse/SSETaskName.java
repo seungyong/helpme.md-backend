b1c9268323c2c20367dd30594c81b888cac7c631
seungyong <seungyong20@naver.com>
2026-02-08 12:28:39 +0900

    SSE 정리

--- helpme-backend/src/main/java/seungyong/helpmebackend/infrastructure/sse/SSETaskName.java
+++ helpme-backend/src/main/java/seungyong/helpmebackend/infrastructure/sse/SSETaskName.java
@@ -6,8 +6,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SSETaskName {
-    COMPLETION_EVALUATE_PUSH("completion-evaluate-push"),
-    COMPLETION_EVALUATE_PUSH_ERROR("completion-evaluate-push-error"),
     COMPLETION_EVALUATE_DRAFT("completion-evaluate-draft"),
     COMPLETION_EVALUATE_DRAFT_ERROR("completion-evaluate-draft-error"),
     COMPLETION_GENERATE("completion-generate"),
